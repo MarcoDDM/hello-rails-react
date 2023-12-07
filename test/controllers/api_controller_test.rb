130c2589a495200bdbc5149f7b53ed48ab57070b
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-12-06 21:10:01 -0300

    Remove test folder unused

--- test/controllers/api_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class ApiControllerTest < ActionDispatch::IntegrationTest
-  test 'should get greetings' do
-    get api_greetings_url
-    assert_response :success
-  end
-end
